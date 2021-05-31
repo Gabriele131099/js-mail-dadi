a4403ac2681445468f0c489ce537914b1d0288f9
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-31 14:58:32 +0200

    struttura

---
+++ script.js
@@ -0,0 +1,46 @@
+// //  function per E-mail
+
+function checkEmail() {
+    var email= ["gabrisanna.131099@gmail.com" , "mariorossi@gmail.com" , "gatanoesposito099@gmail.com" , "yanguh@gmail.com"]
+    var emailUtente = document.getElementById("email").value;
+    var output = document.getElementById("invio")
+
+    // for e if per controllare se la mail è presente
+
+
+    var check = false; 
+
+    for (var i = 0; i < email.length; i++) {
+        if (email[i] === emailUtente) {
+            check = true;
+            break;
+        }
+    }
+    if (check) {
+        output.innerHTML += 'email corretta';
+    } else {
+        output.innerHTML += 'email sbagliatta!';
+    }
+
+}
+
+// function dado
+
+function randomDado() {
+    var output = document.getElementById("run")
+    
+
+
+    var dadoPc= Math.random(0,5)
+    var dadoUtente=Math.random(0,5)
+
+    if (dadoUtente>dadoPc) {
+
+        output.innerHTML= "hai vinto!"
+        
+    } else {
+        output.innerHTML= "hai perso"
+
+    }
+    
+}
